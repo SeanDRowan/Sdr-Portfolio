0516ee09291c7cfc0b3b857c2f5780296ddc39f8
Sean <sdrowan93@gmail.com>
2023-10-21 17:02:24 -0500

    pages added'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import HelloReact from './components/Header';
+import Header from './components/Header';
+import  Body   from './components/Body'
+//import Footer from './components/Footer'
 
 // TODO: Add a comment explaining what this function is doing
 // This function is a functional component that helps us split the UI into distinct parts.
 // In this case, we are returning another component, <HelloReact/> from it.
 function App() {
-  return <HelloReact />;
+  return <div><Header/>
+  ,
+<Body/>,
+
+  </div>
+   ;
 }
 
 export default App;
