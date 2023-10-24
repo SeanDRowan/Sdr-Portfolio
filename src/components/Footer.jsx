72c96d1caa471f1c3155c006f2d2e410e9612b70
Sean <sdrowan93@gmail.com>
2023-10-24 18:56:58 -0500

    once more

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
  return <footer className ="footer">
 <ul>
 <li className='footer1'><a href="mailto:sdrowan93@gmail.com">Sdrowan93@gmail.com</a></li>
-<li className='footer1'> <a href="https://github.com/SeanDRowan"><img height='50px'src="images/github-mark-white.png"/>Git Hub</a></li>
+<li className='footer1'> <a href="https://github.com/SeanDRowan"><img height='50px'src="assets/github-mark-white.png"/>Git Hub</a></li>
 <li className='footer1'><p>Phone number: 210-392-2118</p></li>
 </ul>
 </footer>
--- src/projects.js
+++ src/projects.js
@@ -8,24 +8,24 @@ const projects = [
                 title:'Project-2: rate-my-tutor',
                 link:'https://serene-journey-47318-c81386755a3c.herokuapp.com/',
                 github:'https://github.com/OliveraDiaz/Rate-my-Tutor',
-                img:'images/rate-my-tutor.png',
+                img:'assets/rate-my-tutor.png',
                            },{
                             title:'Weather-Dashboard',
                             link:'https://seandrowan.github.io/weather-Dashboard/',
                             github:'https://oliveradiaz.github.io/Concert-project1/',
-                            img:'images/253394814-d13c19f3-e655-4bf4-9969-2eed49bee19b.png',
+                            img:'assets/253394814-d13c19f3-e655-4bf4-9969-2eed49bee19b.png',
                                        }
                                        ,{
                                         title:'Work Day Scheduler',
                                         link:'https://seandrowan.github.io/Work-day-Schedual/',
                                         github:'https://github.com/SeanDRowan/Work-day-Schedual',
-                                        img:'images/work-day.png',
+                                        img:'assets/work-day.png',
                                                    }
                                                    ,{
                                                     title:'Note Taker',
                                                     link:'https://seandrowan.github.io/note_taker_app/',
                                                     github:'https://github.com/SeanDRowan/note_taker_app',
-                                                    img:'images/Screenshot 2023-10-24 155627.png',
+                                                    img:'assets/Screenshot 2023-10-24 155627.png',
                                                                }
                                                                ,{
                                                                 title:'example3',
